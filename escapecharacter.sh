0996c859ffb8f0a2ef5dc1249e24a6a9537e7070
Prafullakumarsahu <Prafullakumarsahu001@gmail.com>
2020-06-01 18:18:59 +0530

    Add escape characters

---
+++ escapecharacter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Hello \"World\""
+echo "Hello		" World ""
+
+echo * - all file listed with this.
+
+echo "A quote is \", backslash is \\, backtick is \`." #A quote is ", backslash is \, backtick is `.
+echo "A few space are 	 and dollar is \$. \$x is ${x}." #A few space are 	 and dollar is $. $x is .
+echo "This is a \\ backslash" #This is a \ backslash
+echo "This is \" a quote and this is \\ a backslash" #This is " a quote and this is \ a backslash

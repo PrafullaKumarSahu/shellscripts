699f053b8f8537da5f82e42bd2eeab1c7d254b71
Prafullakumarsahu <Prafullakumarsahu001@gmail.com>
2020-06-03 01:42:57 +0530

    Use tr to change case of text

--- hint1.sh
+++ hint1.sh
@@ -3,4 +3,7 @@
 p10user=`grep -i p10user /etc/passwd | cut -d: -f1`
 echo "All users with the word \"p10user\" in their passwd"
 echo "Entries are: "
-echo "$p10user" | tr ' ' '\012'
+echo "$p10user" | tr ' ' '\012' # tr translated the spaces into octal character 012 (NEWLINE)
+
+# Another use of tr is its use of range ...it can convert text to upper or lower case.
+echo "$p10user" | tr ' ' '\012' | tr '[a-z]' '[A-Z]'

6fddb0892aa43e73bbdceeea08cb872dd26965fa
Prafullakumarsahu <Prafullakumarsahu001@gmail.com>
2020-06-03 01:40:19 +0530

    Read all entries

--- hint1.sh
+++ hint1.sh
@@ -2,4 +2,5 @@
 
 p10user=`grep -i p10user /etc/passwd | cut -d: -f1`
 echo "All users with the word \"p10user\" in their passwd"
-echo "Entries are: $p10user"
+echo "Entries are: "
+echo "$p10user" | tr ' ' '\012'
